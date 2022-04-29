2d4292905fca03de05db638ed24e639fd28097e7
Hassan Mohamed <hassan.mhmd656@gmail.com>
2022-04-30 01:15:30 +0200

    fixing a bug & promisify the stripe charge function

--- services/paymentService.js
+++ services/paymentService.js
@@ -1,16 +1,16 @@
 const stripe = require("stripe")(process.env.STRIPE_KEY);
+const util = require("util");
 
-const chargeOrder = (req, res) => {
-  stripe.charges.create(
-    {
-      source: req.body.tokenId,
-      amount: req.body.amount,
-      currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      return { stripeErr, stripeRes };
-    }
-  );
+stripe.charges.create = util.promisify(stripe.charges.create);
+
+const chargeOrder = async (req, res) => {
+  const result = await stripe.charges.create({
+    source: req.body.tokenId,
+    amount: req.body.amount,
+    currency: "usd",
+  });
+
+  return result;
 };
 
 module.exports.paymentService = { chargeOrder };
